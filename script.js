4d1f2c445057e33ad5115258d9edd149176cff6c
coda <roumejons@gmail.com>
2016-09-27 16:08:21 +0200

    exercice fait

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-/* votre fonction ici*/
+fonction changerstyle(){
+
+}
+
+document.getElementById('parag').Style.color=#fff; 
+document.getElementById('parag').Style.backgrond=#000;
+document.getElementById('parag').Style.border=1px oute;
+document.getElementById('parag').Style.padding=spx;
+document.getElementById('parag').Style.backgrond=yellow;
